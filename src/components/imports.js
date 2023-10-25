378463f58b7ee9e46834ae3d67c5a36c02dd5b77
ayodeji <ayodejiakintobi1@gmail.com>
2023-10-25 13:39:29 +0100

    CREATE: IMPORTS.JS

---
+++ src/components/imports.js
@@ -0,0 +1,24 @@
+import NavBar from './NavBar';
+import Billing from './Billing;'
+import Carddeal from './Carddeal;'
+import Business from './Business;'
+import Clients from './Clients'
+import CTA from './CTA'
+import Stats from './Stats;'
+import Footer from './Footer'
+import Testimonals from './Testimonals;'
+import Hero from './Hero'
+
+
+export {
+    NavBar,
+    Billing,
+    Carddeal,
+    Business,
+    Clients,
+    CTA,
+    Stats,
+    Footer,
+    Testimonals,
+    Hero,
+}
