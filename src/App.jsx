4e5d888bc292bbb5e92834625025d4c5defe7928
ayodeji <ayodejiakintobi1@gmail.com>
2023-10-25 13:12:07 +0100

    UPDATE: App comp creared

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
 import React from 'react'
-import styles from './style'
+import style from './style'
 
-const App = () => (
-  <>
-      <h1 className='text-secondary'>
-        <p className={`${styles.padding}`}>Hii i'm saber</p>
-      </h1>
-    </>
-)
+
+const App = () => {
+  return (
+<div className="bg-primary w-full overflow-hidden">
+      <div className={`${style.paddingX} ${style.flexCenter}`}>
+      <div className={`${style.boxWidth}`}>
+        Navbar
+      </div>
+    </div>
+    
+    <div className={`bg-primary ${style.flexCenter}`}>
+      <div className={`${style.boxWidth}`}>
+        Hero
+      </div>
+    </div>
+    <div className={`bg-primary ${style.paddingX} ${style.flexCenter}`}>
+      <div className={`${style.boxWidth}`}>
+        Stats
+        Business
+        Billing
+        CardDeal
+        Testimonials
+        Clients
+        CTA
+        Footer
+      </div>
+
+    </div>
+    
+</div>
+  )
+}
 
 export default App
+
