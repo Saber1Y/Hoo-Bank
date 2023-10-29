2766834e36cb4677c3fa19910573f2bb870ea107
ayodeji <ayodejiakintobi1@gmail.com>
2023-10-29 18:55:43 +0100

    UPDATE: DISPLAYED STATS SECTION

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -7,9 +7,11 @@ const Stats = () => (
     <>
      <section className={`${style.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}>
         {stats.map((item) => (
-            <div key={item.id} className=''>
-              <h3>{item.value}</h3>
-              <p>{item.title}</p>
+            <div key={item.id} className={`flex-1 flex justify-start flex-row items-center m-3` }>
+              <h3 className='font-poppins font-semi-bold xs:text-[40px] 
+              text-[30px] xs:leading-[53px] text-white leading-[43px]'>{item.value}</h3>
+              <p className='font-poppins font-normal xs:text-[20px] 
+              text-[15px] xs:leading-[26px] text-gradient uppercase leading-[21px] ml-3'>{item.title}</p>
             </div>
         ))}
      </section>
