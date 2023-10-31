3bef453bd2d1aa0dc6f4bd71d97ed8d8da4a5a55
ayodeji <ayodejiakintobi1@gmail.com>
2023-10-31 17:24:10 +0100

    UPDATE: CARD COMP

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { quotes } from '../assets'
 
-const FeedbackCard = () => {
+
+const FeedbackCard = ({ id, content, name, title, img }) => {
   return (
-    <div>
-      
+    <div className='flex justify-between flex-col px-10 py-12 
+      rounded-[20px] max-w-[270px] mr-0 my-5 feedback-card md:mr-10 sm:mr-5'>
+       <img src={quotes} alt="quote" />
+       <p className='font-poppins font-normal text-[18px] leading-[32px] text-white my-10 '>{content}</p>
     </div>
   )
 }
--- src/components/Testimonals.jsx
+++ src/components/Testimonals.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import { feedback } from '../constants'
+import styles from '../style'
+import FeedbackCard from './FeedbackCard'
+
 
 const Testimonals = () => {
   return (
-    <div>
-      
-    </div>
+    <section id='clients' className={`${styles.paddingY} ${styles.flexCenter} flex-col relative `}>
+        <div className=''/>
+
+          <div className='w-full flex justify-between items-center 
+           flex-col relative z-[1] sm:mb-16 mb-6'>
+              <h1 className={styles.heading2}>What people are<br className='sm:block hidden' /> saying about us</h1>
+              <div className="w-full mt-6 md:mt-0">
+                <p className={`${styles.paragraph} max-w-[450px] `}>Everything you need to accept card payments and grow your
+                   business anywhere on the planet.</p>
+              </div>
+          </div>
+        <div className='flex flex-wrap justify-center
+         w-full feedback-container relative z-[1]'>
+            {feedback.map((feedback, index) => (
+              <FeedbackCard key={feedback.id} {...feedback} className="w-[42px] h-[27px] object-contain" />
+            ))}
+        </div>
+    </section>
   )
 }
 
