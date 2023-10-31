7edd9389bff15b5c0905fce4cb6d2d8c869a31a6
ayodeji <ayodejiakintobi1@gmail.com>
2023-10-31 22:00:18 +0100

    CREATE & UPDATE: COMP CREATED

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import Button from './Button'
+import styles from '../style'
 
-const CTA = () => {
-  return (
-    <div>
-      
-    </div>
+const CTA = () =>  (
+    <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
+      <div className='flex flex-col flex-1'>
+        <h3 className={styles.heading2}>
+        Let’s try our service now!
+        </h3>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Everything you need to accept card payments and grow 
+          your business anywhere on the planet.</p>
+      </div>
+
+        <div>
+          <Button />
+        </div>
+    </section>
   )
-}
 
 export default CTA
