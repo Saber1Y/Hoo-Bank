8c6f67b081c4a7943e2d690ac6c08b0f031eb882
ayodeji <ayodejiakintobi1@gmail.com>
2023-11-01 00:02:55 +0100

    UPDATE & CREATE: FOOTER COMP

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import styles from '../style'
+import { logo } from '../assets'
+import { footerLinks, socialMedia } from '../constants'
 
-const Footer = () => {
-  return (
-    <div>
+const Footer = () =>(
+    <section>
       
-    </div>
+    </section>
   )
-}
 
 export default Footer
