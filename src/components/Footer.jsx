12211358d8131d8e1579a99839dc9c9bfaead2e2
ayodeji <ayodejiakintobi1@gmail.com>
2023-11-01 00:35:34 +0100

    UPDATE:FOOTER

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -31,6 +31,19 @@ const Footer = () =>(
 
         </div>
       </div>
+
+      <div className='flex w-[full] justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3f3e45]'>
+        <p className='font-poppins font-medium text-white text-[18px] leading-[27px]'>
+        Copyright 2021 HooBank. All Rights Reserved.
+        </p>
+        <div className='flex flex-row md:mt-0 mt-6'>
+          {socialMedia.map((media, index) => (
+            <div key={media.id}>
+              <img src="{media.icon}" alt="{media.id}" className={`w-[21px] h-[21px] object-contain cursor-pointer ${index !== socialMedia.length -1 ? 'mr-6' : 'mr-0'}`}/>
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
   )
 
