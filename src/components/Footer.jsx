bea5d36c5bffc78ee7905f9a4e052d44ae4a7a7b
ayodeji <ayodejiakintobi1@gmail.com>
2023-11-01 00:07:01 +0100

    UPDATE: IMG ADDED

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,12 @@ import { logo } from '../assets'
 import { footerLinks, socialMedia } from '../constants'
 
 const Footer = () =>(
-    <section>
-      
+    <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
+      <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full `}>
+        <div className='flex-1 flex flex-col justify-start mr-10'>
+          <img src={logo} alt="hoobank" className='w-{266px} h-{72px} object-contain' />
+        </div>
+      </div>
     </section>
   )
 
