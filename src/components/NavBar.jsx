c0006ead0f829611675d4d8e0f535c3acd4c35ea
ayodeji <ayodejiakintobi1@gmail.com>
2024-03-05 18:39:16 +0100

    CREATE: REACT ROUTER ADDED

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,16 +2,14 @@ import React from "react";
 import { close, logo, menu } from "../assets";
 import { navLinks } from "../constants";
 import { useState } from "react";
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const NavBar = () => {
   const [toggle, setToggle] = useState(false);
 
-
-
   return (
     <nav className="w-full flex py-2 justify-between items-center navbar">
-      <img src={logo} alt="logo" className=" w-96 h-32" />
+      <img src={logo} alt="logo" className=" w-80 h-32" />
 
       <ul className="list-none sm:flex hidden justify-end items-center flex-1 hover-effect">
         {navLinks.map((nav, index) => (
@@ -53,6 +51,27 @@ const NavBar = () => {
           </ul>
         </div>
       </div>
+      <button
+        type="button"
+        className="text-dark bg-[#a8eff2] hover:bg-[#a8eff2]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#a8eff2]/55 me-2 mb-2"
+      >
+        <svg
+          className="w-4 h-4 me-2"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 18 19"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M8.842 18.083a8.8 8.8 0 0 1-8.65-8.948 8.841 8.841 0 0 1 8.8-8.652h.153a8.464 8.464 0 0 1 5.7 2.257l-2.193 2.038A5.27 5.27 0 0 0 9.09 3.4a5.882 5.882 0 0 0-.2 11.76h.124a5.091 5.091 0 0 0 5.248-4.057L14.3 11H9V8h8.34c.066.543.095 1.09.088 1.636-.086 5.053-3.463 8.449-8.4 8.449l-.186-.002Z"
+            clip-rule="evenodd"
+          />
+        </svg>
+        Sign in
+      </button>
+      <button>Sign Up</button>
+      <button>Login</button>
     </nav>
   );
 };
