9dca901a7f70f9f054bf96cf261bd1d34a552866
Israel <bamideleisrael04@gmail.com>
2024-03-07 17:57:34 +0100

    Test render_template

--- backend/server.py
+++ backend/server.py
@@ -29,31 +29,33 @@ Session(app)
 # Registration route
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    """Register user"""
-    new_filename = "profilePhoto/profilePhoto.svg"
-
-    # User reached route via POST (as by submitting a form via POST)
-    if request.method == "POST":
-        # Declare a list of existing emails
-        rows = cur.execute(
-            "SELECT * FROM users WHERE email = :email",
-            email=request.form.get("email"),
-        )
-        if len(rows) > 0:
-            return apology("email is already taken!")
-
-        # Create email and password variables
-        email = request.form.get("email")
-        hashed_password = generate_password_hash(request.form.get("password"))
-
-        # Insert new registrant into database
-        cur.execute(
-            "INSERT INTO users (email, hash, profilePhoto) VALUES(:email, :hashed_password, :new_filename)",
-            email=email,
-            hashed_password=hashed_password,
-            new_filename=new_filename,
-        )
-        return render_template("LoginForm.jsx")
-
-    else:
-        return render_template("SignForm.jsx")
+    return render_template("LoginForm.jsx")
+    #
+    #"""Register user"""
+    #new_filename = "profilePhoto/profilePhoto.svg"
+#
+    ## User reached route via POST (as by submitting a form via POST)
+    #if request.method == "POST":
+    #    # Declare a list of existing emails
+    #    rows = cur.execute(
+    #        "SELECT * FROM users WHERE email = :email",
+    #        email=request.form.get("email"),
+    #    )
+    #    if len(rows) > 0:
+    #        return apology("email is already taken!")
+#
+    #    # Create email and password variables
+    #    email = request.form.get("email")
+    #    hashed_password = generate_password_hash(request.form.get("password"))
+#
+    #    # Insert new registrant into database
+    #    cur.execute(
+    #        "INSERT INTO users (email, hash, profilePhoto) VALUES(:email, :hashed_password, :new_filename)",
+    #        email=email,
+    #        hashed_password=hashed_password,
+    #        new_filename=new_filename,
+    #    )
+    #    return render_template("LoginForm.jsx")
+#
+    #else:
+    #    return render_template("SignForm.jsx")
