3998e5a9a90017e3dba149f83550b6400f561ab0
Israel <bamideleisrael04@gmail.com>
2024-03-15 01:57:58 +0100

    Disassembled frontend folder

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import style from './style';
+import { NavBar, Hero, Stats, Business, Billing, Carddeal, Testimonals, Clients, CTA, Footer } from './components/imports';
+
+
+const App = () => {
+  return (
+   <div className="bg-primary w-full overflow-hidden">
+      <div className={`${style.paddingX} ${style.flexCenter}`}>
+      <div className={`${style.boxWidth}`}>
+        <NavBar />
+      </div>
+    </div>
+    
+    <div className={`bg-primary ${style.flexCenter}`}>
+      <div className={`${style.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+    <div className={`bg-primary ${style.paddingX} ${style.flexCenter}`}>
+      <div className={`${style.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing />
+        <Carddeal />
+        <Testimonals />
+        <Clients />
+        <CTA />
+        <Footer />
+      </div>
+
+    </div>
+    
+</div>
+  )
+}
+
+export default App
---
+++ src/components/imports.js
@@ -0,0 +1,27 @@
+import NavBar from './NavBar';
+import Billing from './Billing';
+import Carddeal from './Carddeal';
+import Business from './Business';
+import Clients from './Clients';
+import CTA from './CTA';
+import Stats from './Stats';
+import Footer from './Footer';
+import Testimonals from './Testimonals';
+import Hero from './Hero';
+import GetStarted from './GetStarted';
+import Register from './Register';
+
+export {
+    NavBar,
+    Billing,
+    Carddeal,
+    Business,
+    Clients,
+    CTA,
+    Stats,
+    Footer,
+    Testimonals,
+    Hero,
+    GetStarted,
+    Register
+}
