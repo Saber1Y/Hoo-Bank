39db08b92c419388995d214e9361a288a2d188c2
ayodeji <ayodejiakintobi1@gmail.com>
2024-08-27 17:54:20 +0100

    CREATE: REPO-INIT

---
+++ src/main.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
+import Register from "./components/Register.jsx";
+import EroorForm from "./components/EroorForm.jsx";
+import LoginForm from "./components/LoginForm.jsx";
+import Dash from "./components/Dash.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <EroorForm />,
+  },
+  {
+    path: "/Register",
+    element: <Register />,
+  },
+  {
+    path: "/LoginForm",
+    element: <LoginForm />,
+  },
+  {
+    path: "/Dash",
+    element: <Dash />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
